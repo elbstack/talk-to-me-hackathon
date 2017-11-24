0e54db20fba8b219abe0d9fd796fdbee93725684
Sean C. Koop <sean@elbstack.com>
2017-11-24 08:24:59 +0100

    Setup

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+  res.send('Hello World ');
+});
+
+app.listen(3000, () => {
+  console.log('Express started on port 3000');
+});
+
